913b68d6ff8a90e8ab0238c109980c7f09c6d544
gdliba <147038453+gdliba@users.noreply.github.com>
2023-10-05 10:28:09 +0100

    Revert "Update Game1.cs"
    This reverts commit de1ca72809c42b90c9c57f1ef63aee4e70413557.

--- BasicSpaceDude/Game1.cs
+++ BasicSpaceDude/Game1.cs
@@ -15,13 +15,7 @@ namespace BasicSpaceDude
         Background bg;
 
         // An array of dudes
-        
-
-
-        //create a variable called "dudes" that is an array(collection) of
-        //variables of type FloatyDude
-        FloatyDude[] dudes;
-
+        FloatyDude dude;
 
         public Game1()
         {
@@ -32,9 +26,6 @@ namespace BasicSpaceDude
 
         protected override void Initialize()
         {
-            // Scpecify that dudes has space for 100 dudes
-            dudes= new FloatyDude[10000];
-
             base.Initialize();
         }
 
@@ -44,25 +35,17 @@ namespace BasicSpaceDude
 
             bg = new Background(Content.Load<Texture2D>("starfield"));
 
-            for (int i = 0; i < dudes.Length; i++)
-            {
-
-
-                Vector2 startPos = new Vector2(GraphicsDevice.Viewport.Bounds.Center.X + RNG.Next(-100, 100),
-                                                        GraphicsDevice.Viewport.Bounds.Center.Y + RNG.Next(-100, 100));
-                Vector2 startVel = new Vector2((float)(RNG.NextDouble() * 2) - 1,
-                                                        (float)(RNG.NextDouble() * 2) - 1);
-                dudes[i] = new FloatyDude(Content.Load<Texture2D>("dude"+(i%6)), startPos, startVel);
-            }
+            Vector2 startPos = new Vector2(GraphicsDevice.Viewport.Bounds.Center.X + RNG.Next(-100, 100),
+                                                    GraphicsDevice.Viewport.Bounds.Center.Y + RNG.Next(-100, 100));
+            Vector2 startVel = new Vector2((float)(RNG.NextDouble() * 2) - 1,
+                                                    (float)(RNG.NextDouble() * 2) - 1);
+            dude = new FloatyDude(Content.Load<Texture2D>("dude0"), startPos, startVel);
         }
 
         protected override void Update(GameTime gameTime)
         {
+            dude.UpdateMe(GraphicsDevice.Viewport.Bounds);
 
-            for (int i = 0; i < dudes.Length; i++)
-            {
-                dudes[i].UpdateMe(GraphicsDevice.Viewport.Bounds);
-            }
             base.Update(gameTime);
         }
 
@@ -73,10 +56,8 @@ namespace BasicSpaceDude
             _spriteBatch.Begin();
             bg.DrawMe(_spriteBatch);
 
-            for (int i = 0; i < dudes.Length; i++)
-            {
-                dudes[i].DrawMe(_spriteBatch);
-            }
+            dude.DrawMe(_spriteBatch);
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
